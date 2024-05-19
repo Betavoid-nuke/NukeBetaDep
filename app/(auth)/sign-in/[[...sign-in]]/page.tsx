09b54593974890ad6125d1729e92a2be82e8fe15
jaysoni <jaysoni221717@gmail.com>
2024-05-19 05:15:41 -0700

    theam and all

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,13 @@
 import { SignIn } from "@clerk/nextjs";
+// import { SignIn } from "../../../../public/assets/projectPoster2.jpg";
 
 export default function Page() {
-    return <SignIn />;
+    // background: `url('../../../../public/assets/projectPoster2.jpg') center/cover no-repeat`
+    return (
+        
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column'}} className="gradient-background">
+            <div style={{fontSize:'32px', marginBottom:'20px', color:'white', fontWeight:'bold'}}>B E T A V O I D</div>
+            <SignIn />
+        </div>
+    );
 }
