8305339e45f3f526a810d8cbd4983c3863184b24
jaysoni <jaysoni221717@gmail.com>
2024-07-04 16:17:09 -0700

    fix

--- components/NewsAPI/RightbarCards.tsx
+++ components/NewsAPI/RightbarCards.tsx
@@ -28,44 +28,49 @@ interface props {
 
 const RightbarCards = ({alltitles}:props) => {
 
-  const pathname = usePathname();
-  const isHomePage = pathname === '/';
+    const pathname = usePathname();
+    const isHomePage = pathname === '/';
 
-  //articles, all of these are array
-  const titles = alltitles.articles.map(article => article.title);
-  const source = alltitles.articles.map(article => article.source);
-  const description = alltitles.articles.map(article => article.description);
-  const articleurl = alltitles.articles.map(article => article.url);
-  const urlToImage = alltitles.articles.map(article => article.urlToImage);
-  const ariclecontent = alltitles.articles.map(article => article.content);
-  const publishedAt = alltitles.articles.map(article => article.publishedAt);
+    if(alltitles.articles){
 
-  if (isHomePage) {
-    return (
-      <section className="custom-scrollbar rightsidebar">
-        <div className="flex flex-1 flex-col justify-start" style={{ border: '1px', borderColor: '#232323', borderRadius: '20px', borderStyle: 'solid', padding: '20px', overflowY:'hidden' }}>
-          <h3 className="text-heading4-small text-light-1">Suggested Community</h3>
-          
-          <ScrollArea className="text-light-1 h-20 w-full" style={{ overflowWrap: "anywhere", overflowY: "auto", height:'100%', marginTop:'20px' }}>
-            
-          </ScrollArea>
+        //articles, all of these are array
+        const titles = alltitles.articles.map(article => article.title);
+        const source = alltitles.articles.map(article => article.source);
+        const description = alltitles.articles.map(article => article.description);
+        const articleurl = alltitles.articles.map(article => article.url);
+        const urlToImage = alltitles.articles.map(article => article.urlToImage);
+        const ariclecontent = alltitles.articles.map(article => article.content);
+        const publishedAt = alltitles.articles.map(article => article.publishedAt);
 
-        </div>
-        <div className="flex flex-1 flex-col justify-start" style={{ border: '1px', borderColor: '#232323', borderRadius: '20px', borderStyle: 'solid', padding: '20px', overflowY:'hidden' }}>
-          <h3 className="text-heading4-small text-light-1">Latest News</h3>
-          
-          <ScrollArea className="text-light-1 h-20 w-full" style={{ overflowWrap: "anywhere", overflowY: "auto", height:'100%', marginTop:'20px' }}>
-                {titles.map((title, index) => (
-                    <NewsCard title={title} urlarticle={articleurl[index]} />
-                ))}
-          </ScrollArea>
+        if (isHomePage) {
+          return (
+            <section className="custom-scrollbar rightsidebar">
+              <div className="flex flex-1 flex-col justify-start" style={{ border: '1px', borderColor: '#232323', borderRadius: '20px', borderStyle: 'solid', padding: '20px', overflowY:'hidden' }}>
+                <h3 className="text-heading4-small text-light-1">Suggested Community</h3>
 
-        </div>
-      </section>
-    );
-  } else {
-    return <div></div>;
-  }
+                <ScrollArea className="text-light-1 h-20 w-full" style={{ overflowWrap: "anywhere", overflowY: "auto", height:'100%', marginTop:'20px' }}>
+
+                </ScrollArea>
+
+              </div>
+              <div className="flex flex-1 flex-col justify-start" style={{ border: '1px', borderColor: '#232323', borderRadius: '20px', borderStyle: 'solid', padding: '20px', overflowY:'hidden' }}>
+                <h3 className="text-heading4-small text-light-1">Latest News</h3>
+
+                <ScrollArea className="text-light-1 h-20 w-full" style={{ overflowWrap: "anywhere", overflowY: "auto", height:'100%', marginTop:'20px' }}>
+                      {titles.map((title, index) => (
+                          <NewsCard title={title} urlarticle={articleurl[index]} />
+                      ))}
+                </ScrollArea>
+
+              </div>
+            </section>
+          );
+        } else {
+          return <div></div>;
+        }
+    } else {
+        return <div></div>;
+    }
 };
 
 export default RightbarCards;
